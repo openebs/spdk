8c5a14a118fc45dcf1a9a612fe00638852e029ec
Jan Kryl <jan.kryl@mayadata.io>
2019-06-06 13:26:26 +0000

    rpc: allow method handler to accept one void pointer context parameter
    This is needed for our rust-friendly json-rpc method definition.
    The callback passed to spdk_register_method() is a generic function
    which calls the actual handler (rust closure) after parsing the input
    arguments. The context parameter is the rust closure in this case.
    
    The aim of this change is to minimize the diff size as opposed to
    code clarity which is a secondary concern here. By modifying the
    macro used by all method definitions in spdk and supplying a special
    wrapper for spdk methods accepting NULL context parameter, the changes
    needed to be done to spdk code base are fairly minimal.
    
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- test/unit/lib/bdev/raid/bdev_raid.c/bdev_raid_ut.c
+++ test/unit/lib/bdev/raid/bdev_raid.c/bdev_raid_ut.c
@@ -87,7 +87,9 @@ DEFINE_STUB(spdk_bdev_flush_blocks, int, (struct spdk_bdev_desc *desc, struct sp
 DEFINE_STUB(spdk_conf_next_section, struct spdk_conf_section *, (struct spdk_conf_section *sp),
 	    NULL);
 DEFINE_STUB_V(spdk_rpc_register_method, (const char *method, spdk_rpc_method_handler func,
-		uint32_t state_mask));
+		void *ctx, uint32_t state_mask));
+DEFINE_STUB_V(spdk_rpc_no_ctx_wrapper, (struct spdk_jsonrpc_request *request,
+					const struct spdk_json_val *params, void *ctx));
 DEFINE_STUB_V(spdk_rpc_register_alias_deprecated, (const char *method, const char *alias));
 DEFINE_STUB_V(spdk_jsonrpc_end_result, (struct spdk_jsonrpc_request *request,
 					struct spdk_json_write_ctx *w));
