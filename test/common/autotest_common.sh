ff07b6665a184b528265c094c742fe097715c8f8
shubham <shubham.bajpai@mayadata.io>
2021-11-30 16:29:27 +0530

    test: add github actions for tests and push
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -764,7 +764,7 @@ function process_core() {
 		cat <<- BT
 			##### CORE BT ${core##*/} #####
 
-			$(<"$core")
+			$(< "$core")
 
 			--
 		BT
@@ -1561,7 +1561,7 @@ function pap() {
 	while read -r file; do
 		cat <<- FILE
 			--- $file ---
-			$(<"$file")
+			$(< "$file")
 			--- $file ---
 		FILE
 		rm -f "$file"
