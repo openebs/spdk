0761ff891a41d348143bd27edd4d110aec3bfa2a
Dmitry Savitskiy <dmitry.savitskiy@datacore.com>
2023-02-01 18:50:56 +0300

    socket: disabling check for loopback interfaces when creating a socket
    Linux does not support zero copy option for loopback interfaces.
    When SPDK creates a socket, it checks if the socket is opened for loopback
    interface. This check calls getifaddrs function, which sometimes freezes,
    cause I/O processing timeouts.
    
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- module/sock/posix/posix.c
+++ module/sock/posix/posix.c
@@ -530,6 +530,7 @@ posix_sock_create(const char *ip, int port,
 	char buf[MAX_TMPBUF];
 	char portnum[PORTNUMLEN];
 	char *p;
+	char *loopback_check;
 	struct addrinfo hints, *res, *res0;
 	int fd;
 	int rc;
@@ -626,8 +627,16 @@ retry:
 		return NULL;
 	}
 
-	/* Only enable zero copy for non-loopback sockets. */
-	enable_zcopy_user_opts = opts->zcopy && !sock_is_loopback(fd);
+	enable_zcopy_user_opts = opts->zcopy;
+	if (enable_zcopy_user_opts) {
+		/* In certain situations, sock_is_loopback() may take too long, causing freezes,
+		 * which can lead to I/O time outs. */
+		loopback_check = getenv("SOCKET_CREATE_ZERO_COPY_LOOPBACK_CHECK");
+		if (loopback_check != NULL && strcmp(loopback_check, "1") == 0) {
+			/* Only enable zero copy for non-loopback sockets. */
+			enable_zcopy_user_opts = !sock_is_loopback(fd);
+		}
+	}
 
 	sock = posix_sock_alloc(fd, enable_zcopy_user_opts && enable_zcopy_impl_opts, !conn_inprogress);
 	if (sock == NULL) {
