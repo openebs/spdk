18b1c0ce9671cf17f098a9dd147abf62b4827d94
Dmitry Savitskiy <dmitry.savitskiy@datacore.com>
2023-10-03 18:00:51 +0300

    nvmf: callback for subsystem events clean-up
    Cleaning up code for subsystem events
    
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- lib/nvmf/nvmf_internal.h
+++ lib/nvmf/nvmf_internal.h
@@ -359,6 +359,9 @@ struct spdk_nvmf_subsystem {
 	bool						passthrough;
 	/* Subsystem event callback */
 	spdk_nvmf_subsystem_event_cb nvmf_ss_event_cb;
+	/* Subsystem event callback and its argument. */
+	spdk_nvmf_subsystem_event_cb			event_cb_fn;
+	void						*event_cb_arg;
 };
 
 static int
@@ -639,15 +642,4 @@ nvmf_get_transport_poll_group(struct spdk_nvmf_poll_group *group,
  * \return unique controller id or 0xFFFF when all controller ids are in use
  */
 uint16_t nvmf_subsystem_gen_cntlid(struct spdk_nvmf_subsystem *subsystem);
-
-static inline void
-notify_subsystem_events(struct spdk_nvmf_subsystem *subsystem,
-			void *cb_arg,
-			spdk_nvmf_subsystem_events event)
-{
-	if (subsystem->nvmf_ss_event_cb) {
-		subsystem->nvmf_ss_event_cb(subsystem, cb_arg, event);
-	}
-}
-
 #endif /* __NVMF_INTERNAL_H__ */
