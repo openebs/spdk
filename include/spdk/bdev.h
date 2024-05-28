093f8021a1a9fa18c04dd0fd27c02f22afd97a69
Dmitry Savitskiy <dmitry.savitskiy@datacore.com>
2024-05-28 16:13:28 +0300

    bdev: make bdev_nvme_cdw12 and cdw13 packed
    spdk_bdev_ext_io_opts is packed, and its constituents have to be packed too.
    Otherwise, Rust bindgen fails to properly apply align and pack attributes.
    
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- include/spdk/bdev.h
+++ include/spdk/bdev.h
@@ -244,6 +244,7 @@ SPDK_STATIC_ASSERT(sizeof(union spdk_bdev_nvme_ctratt) == 4, "Incorrect size");
  * This is used to pass NVMe specific fields to bdevs, that reports support for them as indicated
  * by \ref spdk_bdev_get_nvme_ctratt
  */
+#pragma pack(push, 1)
 union spdk_bdev_nvme_cdw12 {
 	uint32_t raw;
 
@@ -255,12 +256,14 @@ union spdk_bdev_nvme_cdw12 {
 	} write;
 };
 SPDK_STATIC_ASSERT(sizeof(union spdk_bdev_nvme_cdw12) == 4, "Incorrect size");
+#pragma pack(pop)
 
 /**
  * Union for command dword 13, which by convention matches the NVMe command dword 13 definition.
  * This is used to pass NVMe specific fields to bdevs, that reports support for them as indicated
  * by \ref spdk_bdev_get_nvme_ctratt
  */
+#pragma pack(push, 1)
 union spdk_bdev_nvme_cdw13 {
 	uint32_t raw;
 
@@ -271,6 +274,7 @@ union spdk_bdev_nvme_cdw13 {
 	} write;
 };
 SPDK_STATIC_ASSERT(sizeof(union spdk_bdev_nvme_cdw13) == 4, "Incorrect size");
+#pragma pack(pop)
 
 /**
  * Structure with optional IO request parameters
