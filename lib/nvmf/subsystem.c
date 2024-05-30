0b28d2ffd750f2f1f17deb389bef1697033bc8af
Dmitry Savitskiy <dmitry.savitskiy@datacore.com>
2024-05-30 17:05:44 +0300

    util: fix uses of spdk_posix_file_load_from_name
    Changes for https://review.spdk.io/gerrit/c/spdk/spdk/+/21829 introduced a bug
    in nvmf_ns_reservation_load_json(): non-existing file for ptpl was no longer
    ignored.
    
    spdk_posix_file_load_from_name changed to return an error code, in order to be
    able to distinguish between different error conditions, and to prevent to rely
    on global errno.
    
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -2685,6 +2685,7 @@ static int
 nvmf_ns_reservation_load_json(const struct spdk_nvmf_ns *ns,
 			      struct spdk_nvmf_reservation_info *info)
 {
+	int err;
 	size_t json_size;
 	ssize_t values_cnt, rc;
 	void *json = NULL, *end;
@@ -2700,10 +2701,17 @@ nvmf_ns_reservation_load_json(const struct spdk_nvmf_ns *ns,
 	}
 
 	/* Load all persist file contents into a local buffer */
-	json = spdk_posix_file_load_from_name(file, &json_size);
+	err = spdk_posix_file_load_from_name(file, &json_size, &json);
+
+	/* It's not an error if the file does not exist */
+	if (err == -ENOENT) {
+		SPDK_NOTICELOG("Persist file %s does not exist\n", file);
+		return 0;
+	}
+
 	if (!json) {
-		SPDK_ERRLOG("Load persist file %s failed\n", file);
-		return -ENOMEM;
+		SPDK_ERRLOG("Load persist file %s failed: %s\n", file, strerror(-err));
+		return err;
 	}
 
 	rc = spdk_json_parse(json, json_size, NULL, 0, &end, 0);
