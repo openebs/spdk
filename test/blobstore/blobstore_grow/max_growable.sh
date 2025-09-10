65464cf3ac2e58046ee559c7b991c7d26ce14092
Abhilash Shetty <abhilash.shetty@datacore.com>
2025-09-10 08:40:59 +0000

    test: add test for max growable size of blobstore
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

---
+++ test/blobstore/blobstore_grow/max_growable.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+#  SPDX-License-Identifier: BSD-3-Clause
+#  All rights reserved.
+#
+
+SYSTEM=$(uname -s)
+if [ "$SYSTEM" = "FreeBSD" ]; then
+	echo "max_growable.sh cannot run on FreeBSD currently."
+	exit 0
+fi
+
+testdir="$(readlink -f "$(dirname "$0")")"
+rootdir=$(readlink -f "$testdir"/../../..)
+source "$rootdir/test/common/autotest_common.sh"
+rpc_py="$rootdir/scripts/rpc.py"
+
+"$rootdir"/test/app/bdev_svc/bdev_svc &
+bdev_svc_pid=$!
+
+trap 'killprocess $bdev_svc_pid; exit 1' SIGINT SIGTERM EXIT
+
+waitforlisten $bdev_svc_pid
+
+# LVS, Bdev and aio disk all are cleaned up at the end of grow call.
+
+# Ensures unique diskpath, bdev and lvs name at start.
+disk_unique=$(uuidgen)
+aio_disk="/tmp/$disk_unique.img"
+bdev_name=$(uuidgen)
+lvs_name=$(uuidgen)
+
+function create_lvs() {
+	size_bytes=$1
+	truncate -s "$size_bytes" "$aio_disk"
+	$rpc_py bdev_aio_create "$aio_disk" "$bdev_name" 512
+	$rpc_py bdev_lvol_create_lvstore "$bdev_name" "$lvs_name" -c "$2" --md_pages_per_cluster_ratio="$3"
+}
+
+function grow_lvs() {
+	# Get the max growable size of LVS.
+	cluster_size=$($rpc_py bdev_lvol_get_lvstores -l "$lvs_name" | jq -r '.[0].cluster_size')
+	growable_upto=$(./scripts/rpc.py bdev_lvol_get_lvstores -l "$lvs_name" | jq -r '.[0].max_growable_size')
+	num_blocks=$(./scripts/rpc.py bdev_get_bdevs -b "$bdev_name" | jq -r '.[0].num_blocks')
+	current_size=$((num_blocks * 512))
+	# Grow the underlying device to the max growable size.
+	truncate -s +$((growable_upto - current_size)) "$aio_disk"
+	$rpc_py bdev_aio_rescan "$bdev_name"
+	# Grow the LVS.
+	$rpc_py bdev_lvol_grow_lvstore -l "$lvs_name"
+	# max_growable_size is the absolute growable limit. Even 1 cluster worth of grow now should fail.
+	truncate -s +"$cluster_size" "$aio_disk"
+	$rpc_py bdev_aio_rescan "$bdev_name"
+	expect_fail "$rpc_py" bdev_lvol_grow_lvstore -l "$lvs_name"
+	# Cleanup lvs, bdev and disk.
+	./scripts/rpc.py bdev_lvol_delete_lvstore -l "$lvs_name"
+	./scripts/rpc.py bdev_aio_delete "$bdev_name"
+	rm -f "$aio_disk"
+}
+
+expect_fail() {
+	if "$@"; then
+		echo "FAIL: Grow was not expected to succeed"
+		exit 1
+	else
+		echo "PASS: Grow expected to fail beyond max growable size"
+	fi
+}
+
+# LVS backed by 100GiB device, 4MiB cluster size and 100 mum_md_pages_cluster_ratio.
+create_lvs $((100 * 1024 * 1024 * 1024)) $((4 * 1024 * 1024)) 100
+grow_lvs
+
+# LVS backed by 200GiB device, 8MiB cluster size and 200 mum_md_pages_cluster_ratio.
+create_lvs $((200 * 1024 * 1024 * 1024)) $((8 * 1024 * 1024)) 200
+grow_lvs
+
+# LVS backed by 300GiB device, 16iB cluster size and 300 mum_md_pages_cluster_ratio.
+create_lvs $((300 * 1024 * 1024 * 1024)) $((16 * 1024 * 1024)) 300
+grow_lvs
+
+# LVS backed by 500GiB device, 12iB cluster size and 600 mum_md_pages_cluster_ratio.
+create_lvs $((500 * 1024 * 1024 * 1024)) $((12 * 1024 * 1024)) 600
+grow_lvs
+
+# LVS backed by 1024GiB device, 254GiB cluster size and 800 mum_md_pages_cluster_ratio.
+create_lvs $((1024 * 1024 * 1024 * 1024)) $((254 * 1024 * 1024)) 800
+grow_lvs
+
+killprocess $bdev_svc_pid
+trap - SIGINT SIGTERM EXIT
